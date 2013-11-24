ae4b106f948c889b1571884984081da75151d15b
Luis Buenaventura <luis@syndeolabs.com>
2013-11-24 23:41:27 +0800

    added mongohq and redistogo environment variables

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,4 +2,13 @@ App.redis_config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails
 
 App.redis = Redis.new(App.redis_config)
 
-# Cashier::Adapters::RedisStore.redis = App.redis
+if Rails.env.production?
+
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  
+  # ENV['REDISTOGO_URL'] = "redis://redistogo:ed82059bd102255572cd71c5cc80f41b@grideye.redistogo.com:9483/"
+
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  App.redis = REDIS
+
+end
