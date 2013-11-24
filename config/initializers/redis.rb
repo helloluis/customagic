03dc4e32155baf21f5aa64da630a5e98e630c0bc
Luis Buenaventura <luis@syndeolabs.com>
2013-11-25 04:46:25 +0800

    got redis working for production mode

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,15 +1,14 @@
-App.redis_config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
-
-App.redis = Redis.new(App.redis_config)
 
 if Rails.env.production?
 
   uri = URI.parse("redis://redistogo:ed82059bd102255572cd71c5cc80f41b@grideye.redistogo.com:9483/")
-  # uri = URI.parse(ENV["REDISTOGO_URL"])
-  
-  # ENV['REDISTOGO_URL'] = "redis://redistogo:ed82059bd102255572cd71c5cc80f41b@grideye.redistogo.com:9483/"
-
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   App.redis = REDIS
+
+else
+
+  App.redis_config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
+
+  App.redis = Redis.new(App.redis_config)
 
 end
