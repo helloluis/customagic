abee09aeda13fd9b7cb7c160f30abe5ec1437457
Luis Buenaventura <luis@syndeolabs.com>
2013-11-25 04:32:31 +0800

    replaced env variables with actual URI values

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,7 +4,8 @@ App.redis = Redis.new(App.redis_config)
 
 if Rails.env.production?
 
-  uri = URI.parse(ENV["REDISTOGO_URL"])
+  uri = URI.parse("redis://redistogo:ed82059bd102255572cd71c5cc80f41b@grideye.redistogo.com:9483/")
+  # uri = URI.parse(ENV["REDISTOGO_URL"])
   
   # ENV['REDISTOGO_URL'] = "redis://redistogo:ed82059bd102255572cd71c5cc80f41b@grideye.redistogo.com:9483/"
 
